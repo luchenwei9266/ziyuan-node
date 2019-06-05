0ae40ec0c1bda49d572c5dbae4d1b23fbdf7e38b
root <root@vultr.guest>
2019-06-05 16:20:58 +0800

    new pass

--- routes/getList.js
+++ routes/getList.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'A1140910124',
+    password: '1',
     database: 'DATALIST'
 });
 
